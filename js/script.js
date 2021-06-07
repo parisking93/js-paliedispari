026c89fd513b80835056fbebe4c1e77ec2e3a28f
emmanuelparisi <emmanuel.p2606@gmail.com>
2021-06-07 14:32:09 +0200

    add funzione palindroma

--- js/script.js
+++ js/script.js
@@ -1,1 +1,11 @@
 // Palidroma Chiedere all’utente di inserire una parola. Creare una funzione per capire se la parola inserita è palindroma
+
+function palindroma(parola) {
+    var newParola = '';
+    for (i = parola.length - 1; i >= 0; i--) {
+        newParola += parola[i];
+        console.log(newParola);
+    }
+}
+
+palindroma('otto');
