683608f3e2b224a7062b225c76f7508607263203
emmanuelparisi <emmanuel.p2606@gmail.com>
2021-06-07 16:44:53 +0200

    add button gioco pari e dispari

--- js/script.js
+++ js/script.js
@@ -32,58 +32,63 @@ inserisci.addEventListener('click', start);
 
 // Pari e Dispari L’utente sceglie pari o dispari e inserisce un numero da 1 a 5. Generiamo un numero random (sempre da 1 a 5) per il computer (usando una funzione). Sommiamo i due numeri. Stabiliamo se la somma dei due numeri è pari o dispari (usando una funzione) Dichiariamo chi ha vinto.
 
-// Generiamo un numero random (sempre da 1 a 5) per il computer (usando una funzione).
-function rand(min,max) {
-    var nRandom = Math.floor((Math.random() * ((max + 1) - min) + min))
-    return nRandom
-}
-var pcRandom = rand(1,5);
-
-
-// Pari e Dispari L’utente sceglie pari o dispari e inserisce un numero da 1 a 5.
-var checkCampi = true;
-
-while (checkCampi) {
-    var parOdisp = prompt('scegli pari o dispari');
-    var numero = parseInt(prompt('scegli un numero da 1 a 5'));
-    checkCampi = false;
-    // controllo che i valori inseriti siano correti 
-    if (!(isNaN(parOdisp)) || isNaN(numero)){
-        alert('inserisci i valori corretti nei giusti campi');
-        checkCampi = true;
+function parEdisp() {
+    // Generiamo un numero random (sempre da 1 a 5) per il computer (usando una funzione).
+    function rand(min,max) {
+        var nRandom = Math.floor((Math.random() * ((max + 1) - min) + min))
+        return nRandom
     }
-    if (parOdisp !== 'pari' && parOdisp !== 'dispari') {
-        alert('puoi scegliere solo pari o dispari');
-        checkCampi = true;
+    var pcRandom = rand(1,5);
+
+
+    // Pari e Dispari L’utente sceglie pari o dispari e inserisce un numero da 1 a 5.
+    var checkCampi = true;
+
+    while (checkCampi) {
+        var parOdisp = prompt('scegli pari o dispari');
+        var numero = parseInt(prompt('scegli un numero da 1 a 5'));
+        checkCampi = false;
+        // controllo che i valori inseriti siano correti 
+        if (!(isNaN(parOdisp)) || isNaN(numero)){
+            alert('inserisci i valori corretti nei giusti campi');
+            checkCampi = true;
+        }
+        if (parOdisp !== 'pari' && parOdisp !== 'dispari') {
+            alert('puoi scegliere solo pari o dispari');
+            checkCampi = true;
+        }
+        if (numero < 1 || numero > 5) {
+            alert('il numero deve essere compreso tra 1  e 5');
+            checkCampi = true;
+        }
     }
-    if (numero < 1 || numero > 5) {
-        alert('il numero deve essere compreso tra 1  e 5');
-        checkCampi = true;
+
+
+    // Sommiamo i due numeri.
+    var somma = pcRandom + numero;
+
+
+    // Stabiliamo se la somma dei due numeri è pari o dispari (usando una funzione) Dichiariamo chi ha vinto.
+
+    function checkPoD(number) {
+        if(number % 2 == 0) {
+            return 'pari';
+        } else {
+            return 'dispari';
+        }
     }
-}
 
+    var risult = checkPoD(somma);
+        document.getElementById('somma').innerHTML = 'il tuo numero è ' + numero + '  e hai scelto ' + parOdisp + '<br>' + 'il numero del pc è ' + pcRandom + '<br>' + 'la loro somma è ' + somma ;
 
-// Sommiamo i due numeri.
-var somma = pcRandom + numero;
-
-
-// Stabiliamo se la somma dei due numeri è pari o dispari (usando una funzione) Dichiariamo chi ha vinto.
-
-function checkPoD(number) {
-    if(number % 2 == 0) {
-        return 'pari';
+    if (risult == parOdisp) {
+        document.getElementById('par-disp').innerHTML = 'il numero è ' + risult + ' HAI VINTO';
+        // console.log(pcRandom, numero);
     } else {
-        return 'dispari';
+        document.getElementById('par-disp').innerHTML = 'il numero è ' + risult + ' HAI PERSO';
+        // console.log(pcRandom, numero);
     }
-}
 
-var risult = checkPoD(somma);
-    document.getElementById('somma').innerHTML = 'il tuo numero è ' + numero + '  e hai scelto ' + parOdisp + '<br>' + 'il numero del pc è ' + pcRandom + '<br>' + 'la loro somma è ' + somma ;
-
-if (risult == parOdisp) {
-    document.getElementById('par-disp').innerHTML = 'il numero è ' + risult + ' HAI VINTO';
-    // console.log(pcRandom, numero);
-} else {
-    document.getElementById('par-disp').innerHTML = 'il numero è ' + risult + ' HAI PERSO';
-    // console.log(pcRandom, numero);
 }
+var gioco = document.getElementById('par');
+gioco.addEventListener('click', parEdisp);
