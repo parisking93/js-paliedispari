d908f4d95661bd113a5577ae717ee460f19b521e
emmanuelparisi <emmanuel.p2606@gmail.com>
2021-06-07 16:38:29 +0200

    add click + funzione in palindroma

--- js/script.js
+++ js/script.js
@@ -1,28 +1,32 @@
 // Palidroma Chiedere all’utente di inserire una parola. Creare una funzione per capire se la parola inserita è palindroma
-
-// funzione palindroma 
-function palindroma(parola) {
-    parola = parola.toLowerCase();
-    var newParola = '';
-    for (i = parola.length - 1; i >= 0; i--) {
-        newParola += parola[i]
+function start() {
+    function palindroma(parola) {
+        parola = parola.toLowerCase();
+        var newParola = '';
+        for (i = parola.length - 1; i >= 0; i--) {
+            newParola += parola[i]
+        }
+        if (newParola == parola ) {
+            return [newParola, true];
+        } else {
+            return [newParola, false];
+        }
     }
-    if (newParola == parola ) {
-        return [newParola, true];
+    // input 
+    var pali = prompt('inserisci una parola');
+    
+    // output 
+    var check = palindroma(pali);
+    if (check[1]) {
+        document.getElementById('parola').innerHTML = 'La parola inserita è palindroma : ' + check[0];
     } else {
-        return [newParola, false];
+        document.getElementById('parola').innerHTML = 'La parola inserita non è palindroma : ' + check[0];
     }
 }
-// input 
-var pali = prompt('inserisci una parola');
+var inserisci = document.getElementById('inserisci');
+inserisci.addEventListener('click', start);
+// funzione palindroma 
 
-// output 
-var check = palindroma(pali);
-if (check[1]) {
-    document.getElementById('parola').innerHTML = 'La parola inserita è palindroma : ' + check[0];
-} else {
-    document.getElementById('parola').innerHTML = 'La parola inserita non è palindroma : ' + check[0];
-}
 
 
 
